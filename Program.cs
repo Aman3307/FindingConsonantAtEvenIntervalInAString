ac2ee18f8048a3ead7c0197213ae23065f0e8693
Aman <Aman.Kumar@interactive12.com>
2023-12-27 11:06:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string inputString = Console.ReadLine();
+
+        int length = inputString.Length;
+        int count = 0;
+
+        for (int i = 0; i < length; i += 2)
+        {
+            char currentChar = inputString[i];
+
+            if (IsConsonant(currentChar))
+            //if(!"aeiouAEIOU".Contains(currentChar))   This will also count integers as a consonant.
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Number of consonants at even indices: " + count);
+        Console.ReadKey();
+    }
+
+    static bool IsConsonant(char c)
+    {
+        return char.IsLetter(c) && !"aeiouAEIOU".Contains(c);
+    }
+}
